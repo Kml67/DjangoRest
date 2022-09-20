ee75fa5ef4aab17845894bac6080548f30f49b8f
K.MAKROUM <k.makroum@MacBook-Pro-de-KMAKROUM.local>
2022-09-20 19:15:54 +0200

    premier chapitre fait

---
+++ readme.txt
@@ -0,0 +1,5 @@
+au démarrage
+1. pip install -r requirements.txt 
+2. python3 manage.py init_local_dev 
+3. python3 manage.py migrate
+4. python3 manage.py runserver
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
-from shop.models import Category, Product
-from shop.serializers import CategorySerializer, ProductSerializer
+from shop.models import Category, Product, Article
+from shop.serializers import CategorySerializer, ProductSerializer, ArticleSerializer
 
 
 class CategoryViewset(ReadOnlyModelViewSet):
@@ -19,6 +19,17 @@ class ProductViewset(ReadOnlyModelViewSet):
     def get_queryset(self):
         queryset = Product.objects.filter(active=True)
         category_id = self.request.GET.get('category_id')
-        if category_id:
+        if category_id is not None:
             queryset = queryset.filter(category_id=category_id)
+        return queryset
+
+class ArticleViewset(ReadOnlyModelViewSet):
+
+    serializer_class = ArticleSerializer
+
+    def get_queryset(self):
+        queryset = Article.objects.filter(active=True)
+        product_id = self.request.GET.get('product_id')
+        if product_id is not None:
+            queryset = queryset.filter(product_id=product_id)
         return queryset
